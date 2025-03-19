5e1a846d6b0ebaca688d7ec1bd02212b8c644043
Joaquin Caceres <JOAQUIN.CACERES@correo.ucu.edu.uy>
2025-03-19 20:13:38 -0300

    Cambio incorrecto

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,3 +2,37 @@
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
+// Esta clase implementa la operacion Resta
+public class Substraction
+{
+    public static int Substract(int a, int b)
+    {
+        return a - b;
+    }
+}
+
+// Esta clase implementa la operacion suma
+public class Addition
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+//Esta clase implementa la operacion de Multipliacionm
+
+public class Multiplication
+{
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
+public class Division
+{
+    public static double Divide(int a, int b)
+    {
+        return (double)a / b;   
+    }
+}
