d8f5582a5658b03ef6ddb5b0b81db08cfd46cb65
Joaquin Caceres <JOAQUIN.CACERES@correo.ucu.edu.uy>
2025-03-19 18:50:32 -0300

    Implementación de la operación Multiplicacion

---
+++ src/Program/Multiplicacion.cs
@@ -0,0 +1,12 @@
+namespace Ucu.Poo.GitChallenge;
+
+public class Multiplicacion
+{
+    public class Multiplication
+    {
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
