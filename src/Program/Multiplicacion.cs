f11646e47c4dd0e28e58e3bb9dce815311d16cdd
Joaquin Caceres <JOAQUIN.CACERES@correo.ucu.edu.uy>
2025-03-19 20:15:54 -0300

    "Cambio de commit" Revert "Cambio incorrecto"
    This reverts commit 5e1a846d6b0ebaca688d7ec1bd02212b8c644043.

---
+++ src/Program/Multiplicacion.cs
@@ -0,0 +1,11 @@
+namespace Ucu.Poo.GitChallenge;
+
+//Esta clase implementa la operacion de Multipliacionm
+
+    public class Multiplication
+    {
+        public static int Multiply(int a, int b)
+        {
+            return 0; //Cambio incorrecto
+        }
+    }
