4d674cc45f0c75d61fb547fa37cdfb9742a8dadd
Joaquin Caceres <JOAQUIN.CACERES@correo.ucu.edu.uy>
2025-03-19 20:06:28 -0300

    Cambio incorrecto

--- src/Program/Multiplicacion.cs
+++ src/Program/Multiplicacion.cs
@@ -1,13 +1,11 @@
 namespace Ucu.Poo.GitChallenge;
 
 //Esta clase implementa la operacion de Multipliacionm
-public class Multiplicacion
-{
+
     public class Multiplication
     {
         public static int Multiply(int a, int b)
         {
-            return a * b;
+            return 0; //Cambio incorrecto
         }
     }
-}
