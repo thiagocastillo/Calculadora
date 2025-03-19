152efb9ac513726fe7e927bcde0a7ee19b9da335
thiagocastillo <Thiago.castillo.cbt@gmail.com>
2025-03-19 20:03:18 -0300

    .

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,10 +3,6 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
-Console.WriteLine(Addition.Add(7, 2));
-Console.WriteLine(Substraction.Substract(10, 1));
-Console.WriteLine(Multiplication.Multiply(3, 3));
-Console.WriteLine(Division.Divide(18, 2));
 // Esta clase implementa la operacion Resta
 public class Substraction
 {
