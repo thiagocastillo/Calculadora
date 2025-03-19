a24ef3b375bbc1a3e5b71db9c5074dbe58611ef0
Nicolas Vazquez <Nicolas Vazquez>
2025-03-19 19:17:36 -0300

    Esta clase implementa la operacion suma

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,3 +2,12 @@
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
+
+// Esta clase implementa la operacion resta
+public class Addition
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
